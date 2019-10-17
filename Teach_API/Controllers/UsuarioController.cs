f4ca74f3f779f254f2fe866dbd642e171882ba97
Larissa Martins Mauricio <larissa.jucre@gmail.com>
2019-10-16 21:54:56 -0300

    Criação Projeto API
    Creação Projeto API

---
+++ Teach_API/Controllers/UsuarioController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Teach_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : Controller
+    {
+        public UsuarioController()
+        {
+
+        }
+    }
+}
