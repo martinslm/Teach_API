1217c7ebae0b39f9ebf4a1b33123a8081b4985ea
andersonwagner06 <36700852+andersonwagner06@users.noreply.github.com>
2019-10-20 17:40:29 -0300

    Migração do template vue para a nova aplicação
    Migração do template vue para a nova aplicação

--- Teach_Vue/src/router.js
+++ Teach_Vue/src/router.js
@@ -1,43 +1,63 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Login from './views/Login';
-import SignUp from './views/SignUp';
-import Moredatauser from './views/Moredatauser';
-
+import DashboardLayout from '@/layout/DashboardLayout'
+import AuthLayout from '@/layout/AuthLayout'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
+  linkExactActiveClass: 'active',
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: 'dashboard',
+      component: DashboardLayout,
+      children: [
+        {
+          path: '/dashboard',
+          name: 'dashboard',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "demo" */ './views/Dashboard.vue')
+        },
+        {
+          path: '/icons',
+          name: 'icons',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Icons.vue')
+        },
+        {
+          path: '/profile',
+          name: 'profile',
+          component: () => import(/* webpackChunkName: "demo" */ './views/UserProfile.vue')
+        },
+        {
+          path: '/maps',
+          name: 'maps',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Maps.vue')
+        },
+        {
+          path: '/tables',
+          name: 'tables',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Tables.vue')
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/registrar',
-      name: 'signup',
-      component: SignUp
-    },
-    {
-      path: '/moredatauser',
-      name: 'moredatauser',
-      component: Moredatauser
+      path: '/',
+      redirect: 'login',
+      component: AuthLayout,
+      children: [
+        {
+          path: '/login',
+          name: 'login',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Login.vue')
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Register.vue')
+        }
+      ]
     }
-  ],
+  ]
 })
