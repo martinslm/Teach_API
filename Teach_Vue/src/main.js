d454b11bd6dc8088e912937b8f32f8fc58279d6e
Larissa Martins Mauricio <larissa.jucre@gmail.com>
2019-11-11 13:40:28 -0300

    Alterações
    Alterações

--- Teach_Vue/src/main.js
+++ Teach_Vue/src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store/index'
-import router from './routes/router'
-import DashboardPlugin from './plugins/dashboard-plugin'
-import ApiService from './services/api.service'
-import { VueSpinners } from '@saeris/vue-spinners'
-import './registerServiceWorker'
+ import store from './store/index'
+ import router from './routes/router'
+ import DashboardPlugin from './plugins/dashboard-plugin'
+ import ApiService from './services/api.service'
+ import { VueSpinners } from '@saeris/vue-spinners'
+ import './registerServiceWorker'
 
-Vue.use(DashboardPlugin)
-Vue.use(VueSpinners)
+// Vue.use(DashboardPlugin)
+ Vue.use(VueSpinners)
 
 // Set the base URL of the API
 ApiService.init(process.env.BASE_URL)
